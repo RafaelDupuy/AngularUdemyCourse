6806e4f802efdd8e5dfa60288477142496c57acb
rafaeldupuylome <rafaeldupuylome@gmail.com>
2022-02-03 22:03:44 -0300

    Initial Commit

---
+++ src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, ɵɵsetComponentScope } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+  @Input() name: string = "";
+  @Input() description: string = "";
+  @Input() imagePath: string = "";
+  
+  constructor() {
+   }
+
+  ngOnInit(): void {
+    
+  }
+
+}
